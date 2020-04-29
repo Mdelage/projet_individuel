67d070e6579302405aaf67e509812be3208434ad
Martin Delage <m.delage204@protonmail.com>
2020-04-29 11:20:31 +0200

    This reverts commit 1ae3dc35136665b59b2fbf7e661fd4e530ce1b59.
    Finalement, je préfère utiliser les vues génériques

--- taskmanager/urls.py
+++ taskmanager/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
+from django.contrib.auth.views import LoginView
 
 urlpatterns = [
-    # Login page for users
-    path('login/', views.login, name="login"),
+    # Login page for users, uses the generic class-based view LoginView
+    path('login/', LoginView.as_view(template_name='taskmanager/login.html'), name="login"),
 ]
