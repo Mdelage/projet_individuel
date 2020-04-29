3c9df96f37f4de0c5362bd61e7aae655be5a9e88
Martin Delage <m.delage204@protonmail.com>
2020-04-29 12:01:11 +0200

    Added the Project model, and a way to edit it in the administration

--- taskmanager/models.py
+++ taskmanager/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+# This model represents a project
+class Project(models.Model):
+    name = models.CharField(max_length=300, verbose_name="Nom du projet")
+    users = models.ManyToManyField(User, verbose_name="Participants au projet", related_name="projects")
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+        verbose_name = "projet"
