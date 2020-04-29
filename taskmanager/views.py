3444d0ce53e60c00476e7f878a1c6636037a654d
Martin Delage <m.delage204@protonmail.com>
2020-04-29 12:46:00 +0200

    Modified the projects template and views, so that it displays all projects with their associated users.

--- taskmanager/views.py
+++ taskmanager/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+from .models import *
 
 
 # This view displays all the projects
+@login_required
 def projects(request):
-    return render(request, "taskmanager/projects.html", locals())
+    # This dictionnary contains the lists all of users per project
+    users_per_project = {}
+    for project in Project.objects.all():
+        users_per_project[project] = project.users.all()
+
+    return render(request, "taskmanager/projects.html", {'users_per_project': users_per_project})
